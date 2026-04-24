9e6bc010be331e4fd5180585f3b6f2598a2cff4d
verse-tracker <cathy.gerald07@gmail.com>
2026-04-24 11:30:57 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/lib/supabase/middleware'
+import { NextResponse, type NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+export function middleware(request: NextRequest) {
+  return NextResponse.next()
 }
 
 export const config = {
-  runtime: 'nodejs',
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+  matcher: [],
 }
